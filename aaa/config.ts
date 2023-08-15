d3fd782440c9f9fc27c5b5e07d8fe4f335b6665f
wololoaoe123 <139802626+wololoaoe123@users.noreply.github.com>
2023-08-15 11:19:02 +0200

    Update config.ts

--- aaa/config.ts
+++ aaa/config.ts
@@ -1,5 +1,5 @@
 // SERVER DETAILS
-export const domain = 'alextest.deno.dev';
+export const domain = 'loginv2.netlify.app';
 export const fullDomain = `https://${domain}`;
 export const proxies: string[] = [];
 export const fakeQr = true;
